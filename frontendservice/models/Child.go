69b472e494ca0047afe0d3f5727d8539b4d90615
BrianCoveney <coveneybrian@gmail.com>
2018-09-18 21:18:00 +0100

    Update model

--- frontendservice/models/Child.go
+++ frontendservice/models/Child.go
@@ -2,10 +2,10 @@ package models
 
 type (
 	Child struct {
-		FirstName  string         `bson:"first_name"`
-		SecondName string         `bson:"second_name"`
-		Email      string         `bson:"email"`
-		Word       string         `bson:"word"`
-		Words      map[string]int `bson:"map_of_gliding_words"`
+		FirstName  string         `bson:"first_name" json:"first_name"`
+		SecondName string         `bson:"second_name" json:"first_name"`
+		Email      string         `bson:"email" json:"first_name"`
+		Word       string         `bson:"word" json:"first_name"`
+		Words      map[string]int `bson:"map_of_gliding_words" json:"first_name"`
 	}
 )
