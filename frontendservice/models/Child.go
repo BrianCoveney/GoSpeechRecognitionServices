7f6e7c1540a481ec9d8032b6725fbe8e9ac139fe
BrianCoveney <coveneybrian@gmail.com>
2018-09-25 09:14:46 +0100

    Update Child struct

--- frontendservice/models/Child.go
+++ frontendservice/models/Child.go
@@ -4,7 +4,7 @@ type (
 	Child struct {
 		FirstName  string         `bson:"first_name" json:"first_name"`
 		SecondName string         `bson:"second_name" json:"second_name"`
-		Email      string         `bsodocker n:"email" json:"email"`
+		Email      string         `bson:"email" json:"email"`
 		Word       string         `bson:"word" json:"word"`
 		Words      map[string]int `bson:"map_of_gliding_words" json:"map_of_gliding_words"`
 	}
